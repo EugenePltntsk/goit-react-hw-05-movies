d8c2d02bb1f211de473c4fb993e60595b3197f7b
EugenePltntsk <eugeneplotnytskyi@gmail.com>
2022-11-08 10:20:29 +0200

    one more final version

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import { getTrendingMovies } from 'helpers/API';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import css from "./Home.module.css"
+import css from './Home.module.css';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -21,7 +21,11 @@ export default function Home() {
         {movies.map(item => (
           <li className={css.Lishka} key={item.id}>
             <Link className={css.Link} to={`/movies/${item.id}`}>
-              <img className={css.Image} src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} />
+              <img
+                alt={item.original_title}
+                className={css.Image}
+                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+              />
 
               {item.original_title}
             </Link>
